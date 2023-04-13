47c203279f1c6c74749803569ed56a91a4f18755
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-13 09:03:21 -0400

    fixing amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -25,7 +25,7 @@ def get_amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', strict_slashes=False, methods=['DELETE'])
+@app_views.route('amenities/<amenity_id>', strict_slashes=False, methods=['DELETE'])
 def delete_amenity(amenity_id):
     """Deletes an Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
@@ -38,12 +38,12 @@ def delete_amenity(amenity_id):
 
 @app_views.route('/amenities', strict_slashes=False, methods=['POST'])
 def post_amenity():
-    """Creates an amenity object"""
+    """Creates an Amenity"""
     data = request.get_json()
     if data is None:
-        abort(404, 'Not a JSON')
+        abort(400, 'Not a JSON')
     if 'name' not in data:
-        abort(404, 'Missing name')
+        abort(400, 'Missing name')
     amenity = Amenity(**data)
     amenity.save()
     return make_response(jsonify(amenity.to_dict()), 201)
@@ -52,17 +52,17 @@ def post_amenity():
 @app_views.route('/amenities/<amenity_id>', strict_slashes=False, methods=['PUT'])
 def put_amenity(amenity_id):
     """Updates an Amenity object"""
-    amenities = storage.get(Amenity, amenity_id)
-    if amenities is None:
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
         abort(404)
     data = request.get_json()
     if data is None:
-        abort(404, 'Nt a JSON')
+        abort(400, 'Not a JSON')
     for key, value in data.items():
-        if key not in ['id', 'created_at', 'updated_at', 'state_id']:
-            setattr(amenities, key, value)
-    amenities.save()
-    return make_response(jsonify(amenities.to_dict()), 200)
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(amenity, key, value)
+    amenity.save()
+    return make_response(jsonify(amenity.to_dict()), 200)
 
 
 if __name__ == "__main__":
