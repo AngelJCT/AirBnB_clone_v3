6860bde24a0ea0cef63345acb17428539abc4ba6
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-13 15:02:40 -0400

    adding CORS instance

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module contains the Flask app"""
 from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -8,6 +9,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app)
 
 
 @app.teardown_appcontext
